f7e888498418c5fc7b0e6e0fe5dfc4fc332a9120
Subhajit Sahu <wolfram77@gmail.com>
2016-06-30 23:11:28 +0530

    program: dev & test :honey_pot:
    management object itself refers to a class, not individual fields /
    methods.

--- cmd-brightness/Program.cs
+++ cmd-brightness/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
 
-namespace cmd_brightness {
+namespace orez.obrightness {
 	class Program {
+
+		// data
+		/// <summary>
+		/// WMI Namespace.
+		/// </summary>
+		private static string WmiNs = "root\\WMI";
+
+		/// <summary>
+		/// You have to dream before your dreams can come true.
+		/// : A. P. J. Abdul Kalam
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
 		static void Main(string[] args) {
+			if (args.Length == 0) Get(args);
+			else Set(args);
+		}
+
+		/// <summary>
+		/// Get monitor brightness.
+		/// </summary>
+		/// <param name="args">NA.</param>
+		private static void Get(string[] args) {
+			ManagementObject o = Obj(WmiNs, "WmiMonitorBrightness");
+			Console.WriteLine(o == null ? "-1" : o.GetPropertyValue("CurrentBrightness"));
+		}
+
+		/// <summary>
+		/// Set monitor brightness.
+		/// </summary>
+		/// <param name="args">brightness.</param>
+		private static void Set(string[] args) {
+			byte v = 0;
+			byte.TryParse(args[0], out v);
+			ManagementObject o = Obj(WmiNs, "WmiMonitorBrightnessMethods");
+			if (o == null) Console.Error.WriteLine("err: WMI interfacing failed");
+			else o.InvokeMethod("WmiSetBrightness", new object[] { uint.MaxValue, v });
+		}
+
+		/// <summary>
+		/// Get object of specified Management class.
+		/// </summary>
+		/// <param name="ns">Namespace.</param>
+		/// <param name="cls">Class.</param>
+		/// <returns>Management object.</returns>
+		private static ManagementObject Obj(string ns, string cls) {
+			ManagementScope scp = new ManagementScope(ns);
+			SelectQuery qry = new SelectQuery(cls);
+			using(ManagementObjectSearcher srch = new ManagementObjectSearcher(scp, qry)) {
+				using(ManagementObjectCollection coll = srch.Get()) {
+					foreach (ManagementObject obj in coll) return obj;
+				}
+			}
+			return null;
 		}
 	}
 }
